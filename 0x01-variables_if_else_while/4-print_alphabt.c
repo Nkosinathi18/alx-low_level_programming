6149e166573574f7be0075bd1b9199d01ee567b4
Nkosinathi18 <nkosinathimthembu18@gmail.com>
2023-07-14 12:00:11 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,18 +4,19 @@
 
 /**
  * main - Prints the alphabet without q and e.
- * Return: 0
+ * Return: 0 (Success)
  */
 int main(void)
 {
-int i;
-for (i = 97; i < 123; i++)
-{
-if (i != 101 && i != 113)
-{
-putchar(i);
-}
-}
-putchar('\n');
-return (0);
+	int i;
 
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
