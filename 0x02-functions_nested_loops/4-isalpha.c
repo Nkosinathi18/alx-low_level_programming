55e2ebedceb114c4a16ac71aebf35c7d61d2215c
Nkosinathi18 <nkosinathimthembu18@gmail.com>
2023-07-18 12:00:03 -0700

    Task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,8 @@
 
 /**
  * _isalpha - checks for alphabetic character.
- * Retur: 1 if c is a letter, 0 otherwise
+ * @c: the character to be checked.
+ * Return: 1 if c is a letter, 0 otherwise
  */
 
 int _isalpha(int c)
