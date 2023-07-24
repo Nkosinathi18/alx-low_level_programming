294711ec6bb64e97dde73a8d267699f5d224b691
Nkosinathi18 <nkosinathimthembu18@gmail.com>
2023-07-24 11:30:53 -0700

    Task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,4 +2,25 @@
 
 /**
  * puts_half - prints half of a string, folloewd by a new line
- *
+ * @str: input
+ * Return: half of input
+ */
+
+void puts_half(char *str)
+{
+	int a;
+	int n;
+	int longi;
+
+	longi = 0;
+
+	for (a - 0; str[a] != '\0'; a++)
+		longi++;
+	n = (longi / 2);
+
+	if ((longi % 2) == 1)
+		n = ((longi + 1) / 2);
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar("\n");
+}
