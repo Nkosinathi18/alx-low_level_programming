60d2a16b2491871bbb91d47dd8b4dee79473dc2a
Nkosinathi18 <nkosinathimthembu18@gmail.com>
2023-07-24 09:41:44 -0700

    Task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character followed by a new line
+ * @str: input
+ * Return: print
+ */
+
+void puts2(char *str)
+{
+	int longi = 0;
+	int i = 0;
+	char *y = str;
+	int j;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	i = longi - 1;
+	for (j = 0; j <= i; j++)
+	{
+		if (j % 2 == 0)
+		{
+			_putchar(str[o]);
+		}
+	}
+	_putchar('\n');
+}
