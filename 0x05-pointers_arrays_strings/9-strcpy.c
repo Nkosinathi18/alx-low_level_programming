92148c1c0b58c519e38c0e5abd0d942fefc49ce4
Nkosinathi18 <nkosinathimthembu18@gmail.com>
2023-07-24 11:49:54 -0700

    Task9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,17 +9,18 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int l = 0;
 	int j = 0;
 
-	while (*(src + 1) != '\0')
+	while (*(src + l) != '\0')
 	{
-		i++;
+		l++;
 	}
-	for ( ; j < 1; j++)
+	for ( ; j < l ; j++)
 	{
 		dest[j] = src[j];
 	}
-	dest[i] = '\0';
+	dest[l] = '\0';
 	return (dest);
 }
+
